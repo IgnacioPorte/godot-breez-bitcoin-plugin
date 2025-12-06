05ff168d15594d4415b866eade3f59207574e4e1
Ignacio Porte <ignacio.porte@buda.com>
2025-12-06 00:56:11 -0300

    fix: correct icon format

--- addons/breez_bitcoin/plugin.gd
+++ addons/breez_bitcoin/plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("Breez", "Node", preload("breez_node.gd"), preload("res://icon.svg"))
+	add_custom_type("Breez", "Node", preload("breez_node.gd"), preload("res://icon.png"))
 
 func _exit_tree():
 	remove_custom_type("Breez")
